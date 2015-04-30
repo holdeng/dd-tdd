e072ad2199d4521c798edc9b06d2b9da14d3de0f
holdeng <holdeng@umich.edu>
2015-04-30 14:05:42 -0400

    First two features complete

---
+++ evercraft/src/main/java/ab/evercraft/domain/EvercraftCharacter.java
@@ -0,0 +1,11 @@
+package ab.evercraft.domain;
+
+import lombok.Data;
+
+@Data
+public class EvercraftCharacter {
+    
+    String name;
+    String alignment;
+    
+}
---
+++ evercraft/src/test/java/ab/evercraft/test/domain/EvercraftCharacterTest.java
@@ -0,0 +1,44 @@
+package ab.evercraft.test.domain;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ab.evercraft.domain.EvercraftCharacter;
+
+public class EvercraftCharacterTest {
+    
+    EvercraftCharacter myCharacter = null;
+    public static final String CHARACTER_NAME = "Scott";
+    public static final String VALID_ALIGNMENT = "Good";
+    public static final String INVALID_ALIGNMENT = "Bad Ass";
+    
+    @Before
+    public void setUp() throws Exception {
+        myCharacter = new EvercraftCharacter();
+    }
+    
+    @Test
+    public void canCreateEvercraftCharacter() {
+        assertNotNull(myCharacter);
+    }
+    
+    @Test
+    public void canSetCharacterName() {
+        myCharacter.setName(CHARACTER_NAME);
+        assertEquals(CHARACTER_NAME, myCharacter.getName());
+    }
+    
+    @Test
+    public void canSetValidAlignment() {
+        myCharacter.setAlignment(VALID_ALIGNMENT);
+        assertEquals(VALID_ALIGNMENT, myCharacter.getAlignment());
+    }
+    
+    @Test
+    public void cannotSetInvalidAlignment() {
+        myCharacter.setAlignment(INVALID_ALIGNMENT);
+    }
+}
